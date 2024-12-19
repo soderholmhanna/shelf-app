b4f9faba1839d8961a22faf6a327942a6c493875
Hanna <hms.soderholm@gmail.com>
2024-12-19 11:15:08 +0100

    add book types

--- src/types/Book.types.ts
+++ src/types/Book.types.ts
@@ -1,3 +1,17 @@
-// export type Book = {
-
-// }
+export interface Book {
+    id: string;
+    volumeInfo: {
+        title: string;
+        imageLinks: {
+            thumbnail: string;
+        };
+        authors: string[];
+        categories: string[];
+        description: string;
+        language: string;
+        pageCount: number;
+        publisher: string;
+        subtitle: string;
+        averageRating: number;
+    };
+}
